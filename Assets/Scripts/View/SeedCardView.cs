ace10b8852f128be7968d6fb5b89b6c89ca08902
rrrmaster <dudgus9101@gmail.com>
2023-02-04 16:19:35 +0900

    feat: ㄱ작업

--- Assets/Scripts/View/SeedCardView.cs
+++ Assets/Scripts/View/SeedCardView.cs
@@ -1,4 +1,12 @@
-    public class SeedCardView
+using TMPro;
+using UnityEngine;
+
+public class SeedCardView : MonoBehaviour
+{
+    public TMP_Text countText;
+
+    public void SetCountText(int count, int maxCount)
     {
-        
+        countText.text = $"{count}/{maxCount}";
     }
+}
